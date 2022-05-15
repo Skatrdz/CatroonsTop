8a11fcac37aa8d4c59be72bde4c855014aa8e181
Nikitka <Skatrdq@gmail.com>
2022-05-15 16:17:36 +0300

    Ну теперь есть миграции и модель одна

--- CartoonsTop/CartoonsTop/settings.py
+++ CartoonsTop/CartoonsTop/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*&_qb-8g^kzyia_tb430zmep1ttx1-axk2%g0=g8$0ac9k#hok'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- CartoonsTop/Top10Cartoons/models.py
+++ CartoonsTop/Top10Cartoons/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Top10Cartoons(models.Model):
+    tittle = models.CharField(max_length=255)
+    content = models.TextField(blank=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
+    time_crete = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
