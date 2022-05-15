092a99afffa09dc130a0c6792e432f807e730478
Nikitka <Skatrdq@gmail.com>
2022-05-15 10:46:23 +0300

    Ну штука тут новая, no она скоро будет удалена так что не важно

--- CartoonsTop/Top10Cartoons/views.py
+++ CartoonsTop/Top10Cartoons/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("Top 10 cartoons")
 
-def categories(request):
-    return HttpResponse("<h1>Мультфильмы по категориям</h>")
+def categories(request, categories):
+    return HttpResponse(f"<h1>Мультфильмы по категориям</h1><p>{categories}<p>")
